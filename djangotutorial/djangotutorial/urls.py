b03fecffe74db3498f54ce88902c42cde91fdb65
falci.yan <yanferreira478@gmail.com>
2025-03-03 16:46:48 -0300

    Resolvendo erros de URLs

--- djangotutorial/djangotutorial/urls.py
+++ djangotutorial/djangotutorial/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),  # Aqui, estamos associando a URL raiz à aplicação polls
 ]
