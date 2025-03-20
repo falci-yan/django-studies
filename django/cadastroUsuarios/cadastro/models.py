0e078e1be445c7c7356565bb5ba876845104945c
falci.yan <yanferreira478@gmail.com>
2025-03-20 08:13:12 -0300

    Atualização do html e do models.py

--- django/cadastroUsuarios/cadastro/models.py
+++ django/cadastroUsuarios/cadastro/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class usuarios(models.Model):
+    pass
--- django/cadastroUsuarios/cadastroUsuarios/urls.py
+++ django/cadastroUsuarios/cadastroUsuarios/urls.py
@@ -3,4 +3,5 @@ from cadastro import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios'),
 ]
