13889da1cc6b8b19ece9e4d2b4b61cb2a26e86a1
falci.yan <yanferreira478@gmail.com>
2025-03-21 19:00:16 -0300

    Configuração do banco de dados no views.py

--- django/cadastroUsuarios/cadastro/views.py
+++ django/cadastroUsuarios/cadastro/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def cadastra_usuarios(request):
+
+    novo_usuario = cadastra_usuarios()
+    novo_usuario.nome = request.POST('campo_nome')
+    novo_usuario.idade = request.POST('campo_idade')
+    novo_usuario.save()
+
+    usuarios={
+        'usuarios': cadastra_usuarios.objects.all()
+    }
+
+    #Retornar os dados para a página de listagem de usuários
+    return render(request, 'lista_usuarios.html', cadastra_usuarios)
