314e73676cfedb1d3e946dabe6d37b3412612b43
falci.yan <yanferreira478@gmail.com>
2025-03-21 18:22:36 -0300

    Craido banco de dados e atualizado os arquivos

--- django/cadastroUsuarios/cadastro/views.py
+++ django/cadastroUsuarios/cadastro/views.py
@@ -5,4 +5,16 @@ def home(request):
     return render(request, 'home.html')
 
 def usuarios(request):
-    pass
+    #Salvar os dados os dados no banco de dados.
+    novo_usuario = usuario()
+    novo_usuario.nome = request.POST.get('campo_nome')
+    novo_usuario.idade = request.POST.get('campo_idade')
+
+    novo_usuario.save()
+
+    #Exibir todos os usuários cadastrados em uma nova página. O Django espera um dicionário
+    usuarios ={
+        'usuarios': usuario.objects.all()
+    }
+    #Retornar os dados para a página de listagem de usuários.
+    return render(request, 'lista_usuarios.html', usuarios)
